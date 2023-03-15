cfa759b5f0dcd4438758d81fa30a15c7ba0b249d
Deepanshu Verma <vdepanshu12345@gmail.com>
2023-03-15 18:06:33 +0530

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers:{ passwords: 'users/passwords',registrations: 'users/registrations',confirmations: 'users/confirmations',sessions: 'users/sessions' }
-  get '/orders/index', to: 'orders#index'
-  post '/', to: 'orders#submit'
+  get '/', to: 'orders#index'
+  post '/orders/submit', to: 'orders#submit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
